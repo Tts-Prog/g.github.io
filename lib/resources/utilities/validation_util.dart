de14e0b2f93e45abd45774e113ff6b0a13364d0c
Ebenezer Kolade <kolade@systemspecs.com.ng>
2024-03-07 18:20:12 +0100

    valid util

--- lib/resources/utilities/validation_util.dart
+++ lib/resources/utilities/validation_util.dart
@@ -1,1 +1,14 @@
-class ValidationUtil {}
+class ValidationUtil {
+  static String? validateEmail(String email) {
+    if (email.isEmpty) {
+      return "Email address is required";
+    }
+    if (!RegExp(
+            r"^[a-zA-Z0-9.a-zA-Z0-9!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+        .hasMatch(email)) {
+      return "email address is not valid";
+    }
+
+    return null;
+  }
+}
